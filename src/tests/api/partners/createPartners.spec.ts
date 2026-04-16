0f26698fb4ca98ff98dc221ad5658a4aed0d1711
estefaniamunoz801-ai <estefania.munoz801@pascualbravo.edu.co>
2026-04-15 19:11:59 -0500

    chore: update scripts and restrict playwright for using desktop chrome

--- src/tests/api/partners/createPartners.spec.ts
+++ src/tests/api/partners/createPartners.spec.ts
@@ -29,5 +29,24 @@ test.describe("Create Partner", () => {
 
   });
 
+  //test to create a partner with invalid data
+  test("should not create a partner with invalid data", async () => {
+    const invalidRequests = createPartnerData.invalidRequests;
+
+    const response = await test.step("send a POST request to create a partner with invalid data", async () => {
+      return await partnersService.createPartner(invalidRequests.emptyName);
+    });
+
+    await test.step("validate HTTP status code", async () => {
+      expect(response.status).toBe(400);
+    });
+
+    await test.step("validate error message in response body", async () => {      
+      expect(response.body).toHaveProperty("InvalidPartnerNameError");
+      expect(response.body.InvalidPartnerNameError).toContain("Partner name is required");
+    });
+    
+  });
+
 });
 
